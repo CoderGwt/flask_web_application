1c54da11e44ad59cc87ecc7a9216392ad009db57
CoderGwt <1341680843@qq.com>
2019-10-22 10:32:43 +0800

    新用户提交发送邮件提示

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
+from threading import Thread
+
 from flask import Flask, request, make_response, redirect, \
     abort, render_template, url_for, session, flash
-
 from flask_script import Manager, Shell  # 导入flask-script 支持命令行选项
 from flask_bootstrap import Bootstrap  # 导入 flask_bootstrap 继承Twitter Bootstrap
 from flask_moment import Moment  # 使用Flask-Moment 本地化日期和时间
@@ -9,7 +10,9 @@ from wtforms import StringField, SubmitField, DateTimeField, DateField
 from wtforms.validators import Required, DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
 
+from utils.email_secret import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "thisisthesecretkeyandhardtoguessstring"  # 通过app.config设置密钥
@@ -20,13 +23,50 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flask_data:flaskdata@localhost:
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# 配置邮箱相关信息
+app.config['MAIL_SERVER'] = MAIL_SERVER
+app.config['MAIL_PORT'] = MAIL_PORT
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = MAIL_USERNAME
+app.config['MAIL_PASSWORD'] = MAIL_PASSWORD  # 这个是授权码
+app.config['FLAKSY_MAIL_SUBJECT_PREFIX'] = FLASKY_MAIL_SUBJECT_PREFIX
+app.config['FLASKY_MAIL_SENDER'] = FLASKY_MAIL_SENDER
+app.config['FLASK_ADMIN'] = FLASKY_ADMIN
+
 manager = Manager(app)  # 初始化
 bootstrap = Bootstrap(app)  # 初始化
 moment = Moment(app)  # 初始化
 db = SQLAlchemy(app)  # 初始化SQLAlchemy实例
 migrate = Migrate(app, db)
-
 manager.add_command('db', MigrateCommand)
+
+mail = Mail(app)
+
+
+def send_async_mail(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_mail(to, subject, template, **kwargs):
+    """
+    发送邮件
+    :param to: 接收者的邮箱号
+    :param subject: 邮件主题
+    :param template: 邮件内容模板
+    :param kwargs: 额外参数
+    :return:
+    """
+    msg = Message(subject=app.config['FLAKSY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'],
+                  recipients=to)
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+
+    # 开启线程发送邮件， 使用celery可能是个更加的选择
+    thr = Thread(target=send_async_mail, args=[app, msg])
+    thr.start()
+    return thr
 
 
 class Role(db.Model):
@@ -104,6 +144,10 @@ def index():
             db.session.add(user)
             session['known'] = False
             flash("Nice to meet you !")
+            # todo 如果是新用户就发送邮箱表示欢迎
+            if app.config.get('FLASK_ADMIN'):
+                send_mail(app.config['FLASK_ADMIN'], "New User",
+                          'mail/new_user', user=user)
         else:
             session['known'] = True
             flash("Nice to see you too !")
